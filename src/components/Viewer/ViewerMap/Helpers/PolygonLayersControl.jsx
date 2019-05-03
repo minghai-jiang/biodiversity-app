166bc575946eec163e75829e25cb3e7030244f84
FLangelaar <florian.langelaar@hotmail.com>
2019-05-03 11:43:56 +0200

    No Spectral Polygon Crash Fix

--- src/components/Viewer/ViewerMap/Helpers/PolygonLayersControl.jsx
+++ src/components/Viewer/ViewerMap/Helpers/PolygonLayersControl.jsx
@@ -138,8 +138,8 @@ const PolygonLayersControl = {
       {
         if (props.map.classes[i].timestampNumber === props.timestampRange.end)
         {
-          classes = props.map.classes[i].classes;
-          spectral = props.map.spectral[i].indices;
+          props.map.classes[i] ? classes = props.map.classes[i].classes : classes = undefined;
+          props.map.spectral[i] ? spectral = props.map.spectral[i].indices : spectral = undefined;
           break;
         }
       }
